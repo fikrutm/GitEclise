00ffbddcb631feffd719f841623b79d7626fbf86
fikru <fikrutm@gmail.com>
2016-11-24 09:09:15 -0600

    Initial check in of customer

---
+++ Git/src/com/fikru/Gust.java
@@ -0,0 +1,8 @@
+package com.fikru;
+
+public class Gust {
+
+	public void arrived(){
+	System.out.println("We just arrived");
+	}
+}
---
+++ Git/src/com/fikru/Welcome.java
@@ -0,0 +1,14 @@
+package com.fikru;
+
+public class Welcome {
+	public static void main(String[] arr) {
+		Gust g = new Gust();
+		g.arrived();
+		reception();
+
+	}
+
+	public static void reception() {
+		System.out.println("Welcome how may I help you");
+	}
+}

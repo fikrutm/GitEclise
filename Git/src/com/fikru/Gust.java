3a6685782a01f2560f2770b46def815ebdbb5504
fikru <fikrutm@gmail.com>
2016-11-24 18:46:10 -0600

    Commit gust file

--- Git/src/com/fikru/Gust.java
+++ Git/src/com/fikru/Gust.java
@@ -5,5 +5,7 @@ public class Gust {
 	public void arrived(){
 	System.out.println("We just arrived");
 	System.out.println("Second guest comingg next");
+	System.out.println("now it is gusts turn");
+
 	}
 }
